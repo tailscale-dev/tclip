8f637fc099acfb41bd636a7d4e0824f1e9f12044
jolheiser <john.olheiser@gmail.com>
2023-08-17 22:01:16 -0500

    chore: remove markdown hack
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -509,11 +509,6 @@ WHERE p.id = ?1`
 
 	var rawHTML *template.HTML
 
-	// XXX(Xe): HACK around https://github.com/go-enry/go-enry/pull/154
-	if filepath.Ext(fname) == ".markdown" {
-		lang = "Markdown"
-	}
-
 	var cssClass string
 	if lang != "" {
 		cssClass = fmt.Sprintf("lang-%s", strings.ToLower(lang))
